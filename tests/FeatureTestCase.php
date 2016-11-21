1453367da325be7e1f91e9c6da2705c323b0d59c
Duilio Palacios <i@duilio.me>
2016-11-21 14:33:04 +0000

    Refactor validation test, add seeErrors test helper

--- tests/FeatureTestCase.php
+++ tests/FeatureTestCase.php
@@ -5,4 +5,15 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class FeatureTestCase extends TestCase
 {
     use DatabaseTransactions;
+
+    public function seeErrors(array $fields)
+    {
+        foreach ($fields as $name => $errors) {
+            foreach ((array) $errors as $message) {
+                $this->seeInElement(
+                    "#field_{$name}.has-error .help-block", $message
+                );
+            }
+        }
+    }
 }
--- tests/features/CreatePostsTest.php
+++ tests/features/CreatePostsTest.php
@@ -40,7 +40,9 @@ class CreatePostsTest extends FeatureTestCase
             ->visit(route('posts.create'))
             ->press('Publicar')
             ->seePageIs(route('posts.create'))
-            ->seeInElement('#field_title.has-error .help-block', 'El campo título es obligatorio')
-            ->seeInElement('#field_content.has-error .help-block', 'El campo contenido es obligatorio');
+            ->seeErrors([
+                'title' => 'El campo título es obligatorio',
+                'content' => 'El campo contenido es obligatorio'
+            ]);
     }
 }
