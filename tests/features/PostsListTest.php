95c8db140694ce91c728e2d98ba4e55fa62c6554
Duilio Palacios <i@duilio.me>
2016-11-25 11:00:27 +0000

    Paginate posts

--- tests/features/PostsListTest.php
+++ tests/features/PostsListTest.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Post;
+use Carbon\Carbon;
+
 class PostsListTest extends FeatureTestCase
 {
     function test_a_user_can_see_the_posts_list_and_go_to_the_details()
@@ -13,5 +16,30 @@ class PostsListTest extends FeatureTestCase
             ->see($post->title)
             ->click($post->title)
             ->seePageIs($post->url);
+    }
+
+    function test_the_posts_are_paginated()
+    {
+        // Having...
+        $first = factory(Post::class)->create([
+            'title' => 'Post más antiguo',
+            'created_at' => Carbon::now()->subDays(2)
+        ]);
+
+        $posts = factory(Post::class)->times(15)->create([
+            'created_at' => Carbon::now()->subDay()
+        ]);
+
+        $last = factory(Post::class)->create([
+            'title' => 'Post más reciente',
+            'created_at' => Carbon::now()
+        ]);
+
+        $this->visit('/')
+            ->see($last->title)
+            ->dontSee($first->title)
+            ->click('2')
+            ->see($first->title)
+            ->dontSee($last->title);
     }
 }
