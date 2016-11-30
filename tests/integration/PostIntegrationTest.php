0e62fe448a6511e1261a33ad06c3cb314620547e
Carlos Fernandes <informanimo@gmail.com>
2016-11-30 18:37:07 -0400

    Add integration test for generate url and check page of details of the posts

--- tests/integration/PostIntegrationTest.php
+++ tests/integration/PostIntegrationTest.php
@@ -30,4 +30,24 @@ class PostIntegrationTest extends TestCase
                 $this->assertSame('como-instalar-laravel', $post->slug);
         */
     }
+
+    public function test_a_url_is_generated_and_visit_show_page()
+    {
+        // Having
+        $user = $this->defaultUser();
+
+        $post = factory(Post::class)->make([
+            'title' => 'Como instalar Homestead'
+        ]);
+
+        $user->posts()->save($post);
+
+        // When
+        $this->visit($post->url);
+
+        // Then
+        $this->seePageIs("posts/{$post->id}-{$post->slug}")
+            ->seeInElement('h1', $post->title)
+            ->seeInElement('p', $post->content);
+    }
 }
