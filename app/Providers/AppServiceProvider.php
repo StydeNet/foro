d9447c5b472ba3c3126a59d2e73d53665e75f2ee
Duilio Palacios <i@duilio.me>
2017-03-29 16:00:55 +0100

    Register view composers using the boot method instead

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->registerViewComposers();
     }
 
     /**
@@ -32,8 +32,6 @@ class AppServiceProvider extends ServiceProvider
         }
 
         Carbon::setLocale(config('app.locale'));
-        
-        $this->registerViewComposers();
     }
 
     protected function registerViewComposers()
