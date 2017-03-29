a91d9c677419651c263de65609fb139e3151c6ee
Abel Ponce <vnponce8@gmail.com>
2017-03-29 10:01:38 -0600

    Adding actingAsAnyOne and anyone methods on TestsHelper class

--- tests/TestsHelper.php
+++ tests/TestsHelper.php
@@ -24,4 +24,16 @@ trait TestsHelper
     {
         return factory(Post::class)->create($attributes);
     }
+
+    protected function anyone(array $attributes = [])
+    {
+        return factory(User::class)->create($attributes);
+    }
+
+    protected function actingAsAnyone(array $attributes = [])
+    {
+        $user = $this->anyone($attributes);
+        $this->actingAs($user);
+        return $user;
+    }
 }
