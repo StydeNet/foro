496fb2750e9cfdadf831f57f92a41814974a823f
Duilio Palacios <i@duilio.me>
2017-04-01 22:04:57 +0100

    fix N+1 problem in the posts list

--- app/Http/Controllers/ListPostController.php
+++ app/Http/Controllers/ListPostController.php
@@ -12,6 +12,7 @@ class ListPostController extends Controller
         list($orderColumn, $orderDirection) = $this->getListOrder($request->get('orden'));
 
         $posts = Post::query()
+            ->with(['user', 'category'])
             ->scopes($this->getListScopes($category, $request))
             ->orderBy($orderColumn, $orderDirection)
             ->paginate()
