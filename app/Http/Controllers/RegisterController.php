b2bf151bb4c468e5fed717c8a2b1b0aca92f7371
Luis Chávez <luischa04@gmail.com>
2017-04-13 22:42:24 -0500

    Added validations to register and added confirmation message to the new user

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -14,10 +14,25 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
-        //todo: add validation!
+        $this->validate($request, [
+            'email' => 'required|email|unique:users,email',
+            'username' => 'required',
+            'first_name' => 'required',
+            'last_name' =>'required'
+        ], [
+            'unique' => 'Este correo electrónico ya existe.',
+            'email' => 'Dirección de correo inválida.'
+        ]);
 
         $user = User::create($request->all());
 
         Token::generateFor($user)->sendByEmail();
+
+        return redirect()->route('register_confirmation');
+    }
+
+    public function confirmationMessage()
+    {
+        return view('register.confirm_message');
     }
 }
---
+++ resources/views/register/confirm_message.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Gracias por registrarte</div>
+
+                <div class="panel-body">
+                    Enviamos a tu email un enlace para que inicies sesión
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
