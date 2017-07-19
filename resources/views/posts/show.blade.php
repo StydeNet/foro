345f8126427788cc0eb277b300fca2e0d35c441c
Jose Ángel Salvador Moreno <joseangelsalvador57@gmail.com>
2017-07-20 01:35:54 +0200

    Refactorized post publisher information into a partial

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,16 +9,7 @@
 
     <div class="row">
         <div class="col-md-8">
-            <p>
-                Publicado por <a href="#">{{ $post->user->name }}</a>
-                {{ $post->created_at->diffForHumans() }}
-                en <a href="{{ $post->category->url }}">{{ $post->category->name }}</a>.
-                @if ($post->pending)
-                    <span class="label label-warning">Pendiente</span>
-                @else
-                    <span class="label label-success">Completado</span>
-                @endif
-            </p>
+            @include('posts.publisher', $post)
 
             {!! $post->vote_component !!}
 
