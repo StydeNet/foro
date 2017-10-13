5a9218a89d9f86f0aa853c5887c26f60cd8ce241
Isra Nieto <isra.utch@gmail.com>
2017-10-13 11:11:54 -0600

    User is a owner, lesson 20

--- tests/unit/UserModelTest.php
+++ tests/unit/UserModelTest.php
@@ -1,9 +1,11 @@
 <?php
 use App\Post;
 use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class UserModelTest extends TestCase
 {
+    use DatabaseMigrations;
     function test_user_owner_of_model_owns_the_model()
     {
         $user = new User();
@@ -24,5 +26,30 @@ class UserModelTest extends TestCase
         $post->user_id = 2;
 
         $this->assertFalse($user->owns($post));
+    }
+
+    /** @test */
+    function test_a_user_is_a_owner()
+    {
+        $user= factory(\App\User::class)->create(['id' => 1]);
+
+        $post =  factory(\App\Post::class)->create([
+            'user_id' => $user->id
+        ]);
+
+        $this->assertTrue($user->owns($post));
+    }
+
+    /** @test */
+    function test_a_user_is_not_owner()
+    {
+        $not_owner= factory(\App\User::class)->create(['id' => 1]);
+        $owner = factory(\App\User::class)->create(['id' => 2]);
+
+        $post =  factory(\App\Post::class)->create([
+            'user_id' => $owner->id
+        ]);
+
+        $this->assertFalse($not_owner->owns($post));
     }
 }
