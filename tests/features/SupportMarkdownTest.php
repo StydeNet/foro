bfa2c7ccbc45b8bdde6d149f18f7bd7a90788451
JosepSJ <josepsj@gmail.com>
2018-01-11 19:41:06 +0100

    Update SupportMarkdownTest.php
    Ejercicio unidad 23

--- tests/features/SupportMarkdownTest.php
+++ tests/features/SupportMarkdownTest.php
@@ -53,4 +53,77 @@ class SupportMarkdownTest extends FeatureTestCase
         $this->visit($post->url)
             ->dontSee($xssAttack);
     }
+    
+    //para los comentarios
+
+    function test_5_comments_support_markdown()
+    {
+        $importantText = "Texto muy importante";
+
+        $post = $this->createPost();
+
+        $comment = factory(Comment::class)->create([
+            'comment' => "La primera parte del texto. **$importantText**. La última parte del texto.",
+            'post_id' => $post->id,
+        ]);
+
+        $comment->save();
+
+        $this->visit( $comment->post->url )
+            ->seeInElement('strong', $importantText );
+    }
+
+    function test_6_the_code_in_the_comment_is_escaped()
+    {
+        $xssAttack = "<script>alert('Sharing Code!')</script>";
+
+        $post = $this->createPost();
+
+        $comment = factory(Comment::class)->create([
+            'comment' => "$xssAttack. Normal text.",
+            'post_id' => $post->id,
+        ]);
+
+        $comment->save();
+
+        $this->visit( $comment->post->url )
+            ->dontSee( $xssAttack )
+            ->seeText( 'Normal text.')
+            ->seeText( $xssAttack );
+    }
+
+    function test_7_xss_attack_in_comment()
+    {
+        $xssAttack = "<script>alert('Malicius JS!')</script>";
+
+        $post = $this->createPost();
+
+        $comment = factory(Comment::class)->create([
+            'comment' => "$xssAttack. Normal text.",
+            'post_id' => $post->id,
+        ]);
+
+        $comment->save();
+
+        $this->visit( $comment->post->url )
+            ->dontSee( $xssAttack )
+            ->seeText('Normal text');
+    }
+
+    function test_8_xss_attack_with_html_in_comment()
+    {
+        $xssAttack = "<img src='img.jpeg'>";
+
+        $post = $this->createPost();
+
+        $comment = factory(Comment::class)->create([
+            'comment' => "$xssAttack. Normal text.",
+            'post_id' => $post->id,
+        ]);
+
+        $comment->save();
+
+        $this->visit( $comment->post->url )
+            ->dontSee( $xssAttack );
+    }
 }
